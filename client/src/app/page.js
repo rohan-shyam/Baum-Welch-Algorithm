79a038693405a4edf4193300e2dfd9cb5b4b1a21
Rohan Shyam <rohanshyam9@gmail.com>
2026-02-22 18:12:24 +0530

    changed response fetch URL from Render.com to local development server

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -47,7 +47,7 @@ export default function BaumWelchMenu() {
         pi: parsedPi
       };
 
-      const response = await fetch("https://baum-welch-algorithm-1.onrender.com", {
+      const response = await fetch("http://127.0.0.1:8000/api/run/` ", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
