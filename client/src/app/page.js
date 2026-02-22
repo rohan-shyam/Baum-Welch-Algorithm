50026dd4d1091f897051a26c73b1564e2c381949
Rohan <rohanshyam9@gmail.com>
2026-02-22 18:16:45 +0530

    Fixed typo in page.js

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -47,7 +47,7 @@ export default function BaumWelchMenu() {
         pi: parsedPi
       };
 
-      const response = await fetch("http://127.0.0.1:8000/api/run/` ", {
+      const response = await fetch("http://127.0.0.1:8000/api/run/ ", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
